c47b6d577253d26af50f513814faf5cdaf86908e
Pete Gilbert <pete.gilbert16@gmail.com>
2020-07-10 12:04:04 -0400

    Changed hamburger menu behaviour

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,9 @@ $(".showForm").click(function (e) {
     e.preventDefault
     $("form").removeClass("hidden")
 })
+
+$('body').on('touchmove', function (e) {
+    if (!$('.scroll').has($(e.target)).length) e.preventDefault();
+});
+
+console.log('Hi there!👋 Thought you might come looking here! I am currently available for work. Feel free to email me at pete.gilbert16@gmail.com!')
